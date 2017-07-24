049c38309f727a749b125d7e490f0583f4ce1d24
Kudo Chien <kudo@cloudmosa.com>
2017-07-24 11:22:44 +0800

    Revert "DevSupportManager supports multiples instance."
    This reverts commit 570f9b1d9a1ec2eed2af11bdb3c52bbc802c43e6.

--- ReactWindows/ReactNative.Shared/DevSupport/DevSupportManager.cs
+++ ReactWindows/ReactNative.Shared/DevSupport/DevSupportManager.cs
@@ -23,9 +23,7 @@ namespace ReactNative.DevSupport
     class DevSupportManager : IDevSupportManager, IDisposable
     {
         private const int NativeErrorCookie = -1;
-
-        private static int _instanceCount = 0;
-        private readonly string JSBundleFileName = String.Format("ReactNativeDevBundle-{0}.js", _instanceCount++);
+        private const string JSBundleFileName = "ReactNativeDevBundle.js";
 
 #if WINDOWS_UWP
         private readonly ShakeAccelerometer _accelerometer = ShakeAccelerometer.Instance;
